1ccbea5863f961c5dae0952b06851f6066bcd7d0
bimal varghese <bimalvv2005@gmail.com>
2017-10-15 00:27:34 +0530

    Implemented DDl textboxFor with Lambda expression

--- KudVenkat/Controllers/HomeController.cs
+++ KudVenkat/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using KudVenkat.Models;
 
 namespace KudVenkat.Controllers
 {
@@ -11,10 +12,19 @@ namespace KudVenkat.Controllers
     {
         public ActionResult Index()
         {
-            EmployeeBusinessLayer employeeBusinessLayer = new EmployeeBusinessLayer();
+            Company company = new Company("Pragim");
 
-            List<Employee> employees = employeeBusinessLayer.Employees.ToList();
+            ViewBag.Departments = new SelectList(company.Departments, "Id", "Name");
+            ViewBag.CompanyName = company.CompanyName;
+
             return View();
+        }
+
+        public ActionResult Index1()
+        {
+            Company company = new Company("Pragim");
+
+            return View(company);
         }
 
         public ActionResult About()
