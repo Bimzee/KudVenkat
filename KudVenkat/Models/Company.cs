5400432831e15c50bc023b26e94623474d6a1e8d
bimal varghese <bimalvv2005@gmail.com>
2017-10-15 00:27:58 +0530

    Implemented DDl textboxFor with Lambda expression

---
+++ KudVenkat/Models/Company.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace KudVenkat.Models
+{
+    public class Company
+    {
+        private string _name;
+        public Company(string name)
+        {
+            this._name = name;
+
+        }
+
+        public List<tblDepartment> Departments
+        {
+            get
+            {
+                KudVenkatMVCDataModel db = new KudVenkatMVCDataModel();
+                return db.tblDepartments.ToList();
+            }
+        }
+        public string CompanyName
+        {
+            get
+            {
+                return _name;
+            }
+            set
+            {
+                _name = value;
+            }
+        }
+    }
+}
